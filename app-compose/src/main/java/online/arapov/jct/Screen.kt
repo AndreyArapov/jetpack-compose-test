175b031af13d3b85663437e4f777362bce78905b
Andrey Arapov <andrey@arapov.online>
2023-02-09 14:24:05 +0500

    Add benchmark

--- app-compose/src/main/java/online/arapov/jct/Screen.kt
+++ app-compose/src/main/java/online/arapov/jct/Screen.kt
@@ -9,9 +9,13 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,10 +28,16 @@ private val textStyle = TextStyle.Default.copy(
 )
 
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun MainScreen(list: List<Image>) {
     LazyColumn(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .semantics {
+                testTagsAsResourceId = true
+            }
+            .testTag("lazyColumn")
+            .fillMaxSize()
     ) {
         items(list, key = { it.id }) { image ->
             ImageWithText(image)
---
+++ app-view-bench/src/main/java/online/arapov/jct/FrameTimingBenchmark.kt
@@ -0,0 +1,76 @@
+package online.arapov.jct
+
+import androidx.benchmark.macro.CompilationMode
+import androidx.benchmark.macro.FrameTimingMetric
+import androidx.benchmark.macro.StartupMode
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.Direction
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+private const val ITERATIONS = 10
+private const val PACKAGE_NAME = "online.arapov.jct.view"
+
+@RunWith(AndroidJUnit4::class)
+class FrameTimingBenchmark {
+
+    @get:Rule
+    val benchmarkRule = MacrobenchmarkRule()
+
+    @Test
+    fun scrollListWarm() {
+        benchmarkRule.measureRepeated(
+            packageName = PACKAGE_NAME,
+            metrics = listOf(FrameTimingMetric()),
+            compilationMode = CompilationMode.Full(),
+            startupMode = StartupMode.WARM,
+            iterations = ITERATIONS,
+            setupBlock = {
+                startActivityAndWait()
+            }
+        ) {
+            val column = device.findObject(By.res(packageName, "rv"))
+            column.setGestureMargin(device.displayWidth / 5)
+            repeat(3) { column.fling(Direction.DOWN) }
+        }
+    }
+
+    @Test
+    fun scrollListHot() {
+        benchmarkRule.measureRepeated(
+            packageName = PACKAGE_NAME,
+            metrics = listOf(FrameTimingMetric()),
+            compilationMode = CompilationMode.Full(),
+            startupMode = StartupMode.HOT,
+            iterations = ITERATIONS,
+            setupBlock = {
+                startActivityAndWait()
+            }
+        ) {
+            val column = device.findObject(By.res(packageName, "rv"))
+            column.setGestureMargin(device.displayWidth / 5)
+            repeat(3) { column.fling(Direction.DOWN) }
+        }
+    }
+
+    @Test
+    fun scrollListCold() {
+        benchmarkRule.measureRepeated(
+            packageName = PACKAGE_NAME,
+            metrics = listOf(FrameTimingMetric()),
+            compilationMode = CompilationMode.Full(),
+            startupMode = StartupMode.COLD,
+            iterations = ITERATIONS,
+            setupBlock = {
+                startActivityAndWait()
+            }
+        ) {
+            val column = device.findObject(By.res(packageName, "rv"))
+            column.setGestureMargin(device.displayWidth / 5)
+            repeat(3) { column.fling(Direction.DOWN) }
+        }
+    }
+}
